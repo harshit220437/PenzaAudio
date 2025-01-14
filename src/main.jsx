d20cca8fab4019c6ec7387ca4238340451f90871
harshit220437 <harshit220437@gmail.com>
2025-01-14 16:43:13 +0530

    new change

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';  // Make sure you're using `react-dom/client` for React 18+
 import App from './App';  // Import your main App component
 import './index.css';  // Optionally import global CSS
+import { BrowserRouter } from 'react-router-dom';
 
 // Create a root element and render the App component
 const root = ReactDOM.createRoot(document.getElementById('root'));  // Create a root container
 root.render(
   <React.StrictMode>
+    {/* <BrowserRouter> */}
     <App />  {/* Wrap your App component inside React.StrictMode (optional but recommended) */}
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
